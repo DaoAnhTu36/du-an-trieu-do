b77edd2454f1928089b8746a230c8c35bc51268b
DAOANHTU <trailangnd96@gmail.com>
2024-12-23 16:51:27 +0700

    add utilities libraris

--- shop-food/shop-food-authen/Controllers/AdminController.cs
+++ shop-food/shop-food-authen/Controllers/AdminController.cs
@@ -1,11 +1,20 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using shop_food_authen.Contexts;
+using utility;
 
 namespace shop_food_authen.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/admin")]
     [ApiController]
     public class AdminController : ControllerBase
     {
+        public AdminController() { }
+
+        [HttpPost()]
+        public async Task<ApiResponse> SignUp(AdminDTORequest instance)
+        {
+
+        }
     }
 }
---
+++ shop-food/shop-food-authen/Services/Impl/AdminService.cs
@@ -0,0 +1,6 @@
+﻿namespace shop_food_authen.Services.Impl
+{
+    public class AdminService : IAdminService
+    {
+    }
+}
