56cd103a8867dbacb9255da8150cd58a8fde39ae
tutran <trailangnd96@gmail.com>
2024-12-24 15:53:43 +0700

    verify token

--- shop-food/Infrastructure.ApiCore/Middleware/TokenDecodedMiddleware.cs
+++ shop-food/Infrastructure.ApiCore/Middleware/TokenDecodedMiddleware.cs
@@ -36,72 +36,19 @@ namespace Infrastructure.ApiCore.Middleware
                     var token = new JwtSecurityToken(jwtEncodedString);
                     if (token.Payload != null)
                     {
-                        if (token.Payload.TryGetValue("sub", out object sub) &&
-                            !string.IsNullOrEmpty(sub as string))
+                        var isValidToken = JWTExtensions.ValidateToken(tokenString.Replace("Bearer ",""));
+                        if (isValidToken)
                         {
-                            context.Request.Headers.Add("sub", (string)sub);
-                            _logger.LogInformation($"TOKEN DECODED -> sub {(string)sub}");
+                            await _next(context);
+                            return;
                         }
-
-                        if (token.Payload.TryGetValue("email", out object email) &&
-                            !string.IsNullOrEmpty(email as string))
-                        {
-                            context.Request.Headers.Add("email", (string)email);
-                            _logger.LogInformation($"TOKEN DECODED -> email {(string)email}");
-                        }
-
-                        if (token.Payload.TryGetValue("name", out object name) &&
-                            !string.IsNullOrEmpty(name as string))
-                        {
-                            context.Request.Headers.Add("name", (string)name);
-                            _logger.LogInformation(
-                                $"TOKEN DECODED -> name {(string)name}");
-                        }
-
-                        if (token.Payload.TryGetValue("exp", out object exp) &&
-                            !string.IsNullOrEmpty(exp as string))
-                        {
-                            context.Request.Headers.Add("exp", (string)exp);
-                            _logger.LogInformation(
-                                $"TOKEN DECODED -> exp {(string)exp}");
-                        }
-
-                        if (token.Payload.TryGetValue("iss", out object iss) &&
-                            !string.IsNullOrEmpty(iss as string))
-                        {
-                            context.Request.Headers.Add("iss", (string)iss);
-                            _logger.LogInformation(
-                                $"TOKEN DECODED -> iss {(string)iss}");
-                        }
-
-                        if (token.Payload.TryGetValue("aud", out object aud) &&
-                            !string.IsNullOrEmpty(aud as string))
-                        {
-                            context.Request.Headers.Add("aud", (string)aud);
-                            _logger.LogInformation(
-                                $"TOKEN DECODED -> aud {(string)aud}");
-                        }
-
-                        if (token.Payload.TryGetValue("jti", out object jti) &&
-                            !string.IsNullOrEmpty(jti as string))
-                        {
-                            context.Request.Headers.Add("jti", (string)jti);
-                            _logger.LogInformation(
-                                $"TOKEN DECODED -> jti {(string)jti}");
-                        }
-
-                        context.Response.StatusCode = StatusCodes.Status417ExpectationFailed;
-                        await context.Response.WriteAsync("Unauthorized");
                     }
                 }
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                await context.Response.WriteAsync("Unauthorized");
             }
             catch (Exception)
             {
-                // ignored
-            }
-            finally
-            {
-                await _next(context);
             }
         }
     }
