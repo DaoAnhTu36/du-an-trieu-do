e06e7889978b9455a3e0aedb04471c81249b40f6
DAOANHTU <trailangnd96@gmail.com>
2024-12-28 01:37:22 +0700

    [UPDATE] deploy IIS

--- shop-food/shop-food-api/Program.cs
+++ shop-food/shop-food-api/Program.cs
@@ -1,43 +1,49 @@
 using Common.Model.Config;
 using Infrastructure.ApiCore;
 using Infrastructure.ApiCore.Middleware;
-using Serilog;
 using shop_food_api.DatabaseContext;
 using shop_food_api.Repositories;
 using shop_food_api.Services;
-
-var builder = WebApplication.CreateBuilder(args);
-var env = builder.Environment.EnvironmentName;
-IConfiguration configuration = new ConfigurationBuilder()
-        .AddJsonFile("appsettings.json", true, true)
-        .AddJsonFile($"appsettings.{env}.json", true, true)
-        .AddJsonFile($"logsettings.json", true, true)
-        .AddEnvironmentVariables()
-        .Build();
-// Add services to the container.
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGenCustom();
-builder.Services.AddDbContext<EntityDBContext>();
-builder.Services.AddInfrastructures();
-builder.Services.AddScopedServices(ServiceAssembly.Assembly);
-builder.Services.AddScopedRepositories(RepositoryAssembly.Assembly);
-builder.Services.AddScopedUnitOfWorkCore<EntityDBContext>(ServiceExtensions.OverWriteConnectString(configuration.GetSection("AppConfig")));
-builder.Services.Configure<AppConfig>(configuration.GetSection("AppConfig"));
-builder.Services.AddLog();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    var builder = WebApplication.CreateBuilder(args);
+    var env = builder.Environment.EnvironmentName;
+    IConfiguration configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", true, true)
+            .AddJsonFile($"appsettings.{env}.json", true, true)
+            .AddJsonFile($"logsettings.json", true, true)
+            .AddEnvironmentVariables()
+            .Build();
+    // Add services to the container.
 
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-app.UseMiddleware<TokenDecodedMiddleware>();
-app.Urls.Add("http://localhost:1112");
-app.Run();
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGenCustom();
+    builder.Services.AddDbContext<EntityDBContext>();
+    builder.Services.AddInfrastructures();
+    builder.Services.AddScopedServices(ServiceAssembly.Assembly);
+    builder.Services.AddScopedRepositories(RepositoryAssembly.Assembly);
+    builder.Services.AddScopedUnitOfWorkCore<EntityDBContext>(ServiceExtensions.OverWriteConnectString(configuration.GetSection("AppConfig")));
+    builder.Services.Configure<AppConfig>(configuration.GetSection("AppConfig"));
+    builder.Services.AddLog();
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+    app.UseMiddleware<TokenDecodedMiddleware>();
+    //app.Urls.Add("http://localhost:1112");
+    app.Run();
+}
+catch (Exception ex)
+{
+    Console.WriteLine("tutv19");
+    Console.WriteLine(ex.Message);
+}
--- shop-food/shop-food-authen/Program.cs
+++ shop-food/shop-food-authen/Program.cs
@@ -1,42 +1,48 @@
 using Common.Model.Config;
 using Infrastructure.ApiCore;
-using Infrastructure.ApiCore.Middleware;
-using Microsoft.Extensions.Configuration;
 using shop_food_authen.Contexts;
 using shop_food_authen.Services;
 
-var builder = WebApplication.CreateBuilder(args);
-var env = builder.Environment.EnvironmentName;
-IConfiguration configuration = new ConfigurationBuilder()
-    .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json", true, true)
-    .AddJsonFile($"appsettings.{env}.json", true, true)
-    .AddEnvironmentVariables()
-    .Build();
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<EntityDBContext>();
-builder.Services.AddInfrastructures();
-builder.Services.AddScopedServices(ServiceAssembly.Assembly);
-builder.Services.Configure<AppConfig>(configuration.GetSection("AppConfig"));
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+    var env = builder.Environment.EnvironmentName;
+    IConfiguration configuration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", true, true)
+        .AddJsonFile($"appsettings.{env}.json", true, true)
+        .AddEnvironmentVariables()
+        .Build();
+
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddDbContext<EntityDBContext>();
+    builder.Services.AddInfrastructures();
+    builder.Services.AddScopedServices(ServiceAssembly.Assembly);
+    builder.Services.Configure<AppConfig>(configuration.GetSection("AppConfig"));
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    //app.UseMiddleware<AuthenticationMiddleware>();
+    //app.UseMiddleware<TokenDecodedMiddleware>();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-//app.UseMiddleware<AuthenticationMiddleware>();
-//app.UseMiddleware<TokenDecodedMiddleware>();
-
-app.Run("http://localhost:1111");
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    Console.WriteLine("tutv19");
+}
